30ad204cfdae01c709c937bbf29167a1a642e64a
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-30 06:41:46 +0300

    Update feature titles to uppercase

--- app/features.ts
+++ app/features.ts
@@ -4,32 +4,27 @@ export const features = [
     description:
       "Alanya merkezli firmamız, 2023’te kurulan Aqua Active markasıyla, Aqua Jump, Aqua Step, Aqua Spinning, Aqua Fitness, Kangoo Jumps gibi çeşitli spor branşlarında eğlenceli ve yenilikçi deneyimler sunmayı amaçlamaktadır. Müşteri memnuniyetini temel ilke olarak benimseyen uzman ekibimizle Alanya’da spor ve sağlıklı yaşam konusunda öncü bir rol oynamayı hedefliyoruz. Turizmde değer sunmanın yanı sıra uluslararası alanda spor ve sağlıklı yaşam konusunda öncü bir marka olma vizyonuyla ilerliyoruz.",
     src: "/images/1.webp",
-    color: "#BBACAF",
   },
   {
-    title: "Misyonumuz",
+    title: "MISYONUMUZ",
     description:
       "Aqua Active, sporu yaşam tarzının temel bir unsuru olarak benimseyen bir şirkettir. Misyonumuz, uzman eğitmen kadromuz ve çeşitli spor branşlarımız ile her bireyin fiziksel, zihinsel ve sosyal bienallerine katkı sağlamak, keyifli ve etkili antrenman deneyimleri sunmaktır. Müşteri memnuniyetini temel ilke olarak benimseyerek, sağlıklı bir gelecek için bireyleri motive etmeyi ve rehberlik etmeyi amaçlıyoruz.",
     src: "/images/1.webp",
-    color: "#977F6D",
   },
   {
-    title: "Vizyonumuz",
+    title: "VIZYONUMUZ",
     description:
       "Vizyonumuz ise sporun gücünü kullanarak toplumda sağlıklı yaşam alışkanlıklarını yaygınlaştırmak, yaratıcı çözümlerle dolu, sürdürülebilir ve yenilikçi spor deneyimleri sunarak ulusal ve uluslararası alanda öncü bir marka olmayı hedeflemektir. Aqua Active, müşterilerimizin sporu sevmelerini, yaşamlarına keyif ve enerji katmalarını sağlamak için var gücüyle çalışan, dinamik ve motive bir topluluğun parçası olmayı amaçlamaktadır.",
     src: "/images/1.webp",
-    color: "#C2491D",
   },
   {
-    title: "Neden Alanya?",
+    title: "NEDEN ALANYA?",
     description: "Alanya, Antalya’nın güzellikleriyle tanıştığı bir turistik cennet. Aqua Active olarak, Alanya’nın enerjisiyle buluşarak çeşitli spor branşlarıyla sağlıklı yaşamı destekliyoruz. Aqua Jump, Aqua Step, Aqua Spinning, Aqua Fitness, Kangoo Jumps gibi aktivitelerimizle Alanya’da sporun ve sağlıklı yaşamın öncüsü olma misyonumuzu sürdürüyoruz. Uzman eğitmen kadromuzla sizleri Alanya’da sağlıklı bir yaşama davet ediyoruz.",
     src: "/images/1.webp",
-    color: "#B62429",
   },
   {
-    title: "Bizimle Çalışmak İster Misiniz?",
+    title: "BIZIMLE CALISMAK ISTER MISINIZ?",
     description: "Aqua Active olarak, sizlere benzersiz spor deneyimleri sunmak ve sağlıklı yaşamı desteklemek amacıyla profesyonel ekibimiz ve çeşitli spor branşlarımızla hizmetinizdeyiz. Siz de sağlıklı bir yaşam tarzını benimseyen, dinamik ve enerjik bir ortak arıyorsanız, Aqua Active ile çalışmayı düşünmenizi öneririz.\n Bize ulaşarak detaylı bilgi alabilir, ihtiyaçlarınıza yönelik özel spor etkinlikleri ve gösteriler tasarlamak için birlikte çalışma fırsatını değerlendirebiliriz. Sizinle birlikte, sporun gücünü kullanarak unutulmaz ve etkileyici deneyimler yaratmak için sabırsızlanıyoruz.",
     src: "/images/1.webp",
-    color: "#88A28D",
   },
 ];
