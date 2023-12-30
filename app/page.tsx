204e85e91469b121f5e21dde64e9e333a4fe672e
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-30 06:14:13 +0300

    Add Cards component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image, { StaticImageData } from "next/image"
 import Gallery from "./components/Gallery"
 import Banner from "./components/Banner"
+import Cards from "./components/Cards"
 export default function Home() {
 
   return (
-    <main className="overflow-hidden bg-slate-700 relative">
+    <main className="bg-slate-700">
       <Banner />
       <Gallery />
+      <Cards />
     </main>
   )
 }
