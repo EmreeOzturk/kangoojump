b55e3af982d2451172ee05e90d39d3ee798e994d
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-30 05:08:54 +0300

    Add 'no-scrollbar' class to html element

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='antialiased'>
+    <html lang="en" className='antialiased no-scrollbar'>
       <body className={acme.className}>
         <Header />
         <PageTransitionProvider>
