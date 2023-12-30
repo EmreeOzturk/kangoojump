4c041d30b752ffba3b254a24044aad7d5b0bc341
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-30 06:45:43 +0300

    Update feature images

--- app/features.ts
+++ app/features.ts
@@ -9,22 +9,22 @@ export const features = [
     title: "MISYONUMUZ",
     description:
       "Aqua Active, sporu yaşam tarzının temel bir unsuru olarak benimseyen bir şirkettir. Misyonumuz, uzman eğitmen kadromuz ve çeşitli spor branşlarımız ile her bireyin fiziksel, zihinsel ve sosyal bienallerine katkı sağlamak, keyifli ve etkili antrenman deneyimleri sunmaktır. Müşteri memnuniyetini temel ilke olarak benimseyerek, sağlıklı bir gelecek için bireyleri motive etmeyi ve rehberlik etmeyi amaçlıyoruz.",
-    src: "/images/1.webp",
+    src: "/images/2.webp",
   },
   {
     title: "VIZYONUMUZ",
     description:
       "Vizyonumuz ise sporun gücünü kullanarak toplumda sağlıklı yaşam alışkanlıklarını yaygınlaştırmak, yaratıcı çözümlerle dolu, sürdürülebilir ve yenilikçi spor deneyimleri sunarak ulusal ve uluslararası alanda öncü bir marka olmayı hedeflemektir. Aqua Active, müşterilerimizin sporu sevmelerini, yaşamlarına keyif ve enerji katmalarını sağlamak için var gücüyle çalışan, dinamik ve motive bir topluluğun parçası olmayı amaçlamaktadır.",
-    src: "/images/1.webp",
+    src: "/images/3.webp",
   },
   {
     title: "NEDEN ALANYA?",
     description: "Alanya, Antalya’nın güzellikleriyle tanıştığı bir turistik cennet. Aqua Active olarak, Alanya’nın enerjisiyle buluşarak çeşitli spor branşlarıyla sağlıklı yaşamı destekliyoruz. Aqua Jump, Aqua Step, Aqua Spinning, Aqua Fitness, Kangoo Jumps gibi aktivitelerimizle Alanya’da sporun ve sağlıklı yaşamın öncüsü olma misyonumuzu sürdürüyoruz. Uzman eğitmen kadromuzla sizleri Alanya’da sağlıklı bir yaşama davet ediyoruz.",
-    src: "/images/1.webp",
+    src: "/images/4.webp",
   },
   {
     title: "BIZIMLE CALISMAK ISTER MISINIZ?",
     description: "Aqua Active olarak, sizlere benzersiz spor deneyimleri sunmak ve sağlıklı yaşamı desteklemek amacıyla profesyonel ekibimiz ve çeşitli spor branşlarımızla hizmetinizdeyiz. Siz de sağlıklı bir yaşam tarzını benimseyen, dinamik ve enerjik bir ortak arıyorsanız, Aqua Active ile çalışmayı düşünmenizi öneririz.\n Bize ulaşarak detaylı bilgi alabilir, ihtiyaçlarınıza yönelik özel spor etkinlikleri ve gösteriler tasarlamak için birlikte çalışma fırsatını değerlendirebiliriz. Sizinle birlikte, sporun gücünü kullanarak unutulmaz ve etkileyici deneyimler yaratmak için sabırsızlanıyoruz.",
-    src: "/images/1.webp",
+    src: "/images/5.webp",
   },
 ];
