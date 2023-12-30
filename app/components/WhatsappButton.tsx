09c8db234a163e6d0b1d554f67b339ff4f8d49af
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-30 14:14:21 +0300

    Add scroll animation to WhatsappButton component

--- app/components/WhatsappButton.tsx
+++ app/components/WhatsappButton.tsx
@@ -1,12 +1,57 @@
+"use client";
+import { useState, useEffect } from 'react'
+import { motion, useScroll } from 'framer-motion'
+import { usePathname } from 'next/navigation';
 import Image from 'next/image'
 import Link from 'next/link'
 const WhatsappButton = () => {
+    const path = usePathname()
+    const [show, setShow] = useState(false)
+    const { scrollY } = useScroll()
+    const handleScroll = () => {
+        if (scrollY.get() > 50) {
+            setShow(true)
+        } else {
+            setShow(false)
+        }
+    }
+    useEffect(() => {
+        handleScroll()
+        scrollY.on("change", handleScroll)
+        //eslint-disable-next-line
+    }, [scrollY])
+    if (path !== "/") {
+        return (
+            <div className='h-16 w-16 rounded-full cardWP fixed flex items-center justify-center bottom-6 right-6 z-20'>
+                <Link href="https://wa.me/905439466" target="_blank" rel="noopener noreferrer">
+                    <Image src='/wp.webp' alt='whatsapp' width={36} height={36} />
+                </Link>
+            </div>
+        )
+    }
     return (
-        <div className='h-16 w-16 rounded-full cardWP fixed flex items-center justify-center bottom-6 right-6 z-20'>
+        <motion.div
+            variants={
+                {
+                    visible: {
+                        opacity: 1,
+
+                    },
+                    hidden: {
+                        opacity: 0
+                    }
+                }
+            }
+            initial="hidden"
+            animate={
+                show ? "visible" : "hidden"
+            }
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            className='h-16 w-16 rounded-full cardWP fixed flex items-center justify-center bottom-6 right-6 z-20'>
             <Link href="https://wa.me/905439466" target="_blank" rel="noopener noreferrer">
                 <Image src='/wp.webp' alt='whatsapp' width={36} height={36} />
             </Link>
-        </div>
+        </motion.div>
     )
 }
 
