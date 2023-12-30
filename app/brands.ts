6f44d9849229776e8d321191ec43ae6cab094421
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-30 07:01:56 +0300

    Add brands.ts and Referances.tsx files

---
+++ app/brands.ts
@@ -0,0 +1,29 @@
+import bir from "@/public/brands/1.webp";
+import iki from "@/public/brands/2.webp";
+import uc from "@/public/brands/3.webp";
+import dort from "@/public/brands/4.webp";
+import bes from "@/public/brands/5.webp";
+
+export const brands = [
+  {
+    id: 1,
+    image: bir,
+  },
+  {
+    id: 2,
+    image: iki,
+  },
+  {
+    id: 3,
+    image: uc,
+  },
+  {
+    id: 4,
+    image: dort,
+  },
+  {
+    id: 5,
+    image: bes,
+  },
+];
+
