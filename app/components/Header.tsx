a53378a23536e8a4ba8a5579319f8e1eaec8688c
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-30 14:06:11 +0300

    Fix import statement in Header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from './Navbar'
 import Logo from './Logo'
 import { motion, useScroll } from 'framer-motion'
