90b3ef9d76fb13f8b3de41447e969dc4af885f2b
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-30 05:09:26 +0300

    Add link to logo image

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '@/public/images/logo.webp'
 import Image from 'next/image'
+import Link from 'next/link'
 interface LogoProps {
     size?: 'sm' | 'lg'
 }
@@ -11,7 +12,9 @@ const Logo: React.FC<LogoProps> = (
     const h = size === 'sm' ? 50 : 150
     return (
         <div className="flex items-center gap-2">
-            <Image src={logo} alt="logo" width={w} height={h} />
+            <Link href="/">
+                <Image src={logo} alt="logo" width={w} height={h} />
+            </Link>
         </div>
     )
 }
