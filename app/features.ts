b3059663a0ce0e830fa2c32d36b12bfe2077f702
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-30 06:14:06 +0300

    Add new features to app

---
+++ app/features.ts
@@ -0,0 +1,26 @@
+export const features = [
+  {
+    title: "Feature 1",
+    description: "This is the first feature",
+    src: "/images/1.webp",
+    color: "#BBACAF",
+  },
+  {
+    title: "Feature 2",
+    description: "This is the first feature 2",
+    src: "/images/1.webp",
+    color: "#977F6D",
+  },
+  {
+    title: "Feature 3",
+    description: "This is the first feature 3",
+    src: "/images/1.webp",
+    color: "#C2491D",
+  },
+  {
+    title: "Feature 4",
+    description: "This is the first feature 4",
+    src: "/images/1.webp",
+    color: "#B62429",
+  },
+];
