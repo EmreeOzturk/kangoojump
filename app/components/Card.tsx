50d990f35c9e7e731f070f58f76d9b47d854dae3
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-30 06:26:29 +0300

    Refactor Card component styles

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -31,17 +31,12 @@ const Card: React.FC<CardProps> = (
     const scale = useTransform(progress, range, [1, targetScale]);
     return (
         <div ref={container} className='h-screen flex items-center justify-center sticky top-0'>
-            <motion.div style={{ backgroundColor: feature.color, scale, top: `calc(-5vh + ${i * 25}px)` }} className='flex flex-col relative -top-[25%] h-[500px] w-[1000px] rounded-2xl p-[50px] origin-top'>
+            <motion.div style={{ backgroundColor: feature.color, scale, top: `calc(-5vh + ${i * 25}px)` }} className='flex flex-col relative -top-[25%]  h-[600px] w-[1000px] rounded-2xl p-[50px] origin-top'>
                 <h2 className='text-center text-[28px]'>{feature.title}</h2>
                 <div className="flex h-full mt-[50px] gap-12">
                     <div className="w-[40%] relative top-[10%]">
                         <p className='text-lg first-letter:text-[28px] first-letter:font-serif'>{feature.description}</p>
-                        <span className='flex items-center gap-1'>
-                            <Link className='text-[12px] underline cursor-pointer' href="/" target="_blank">See more</Link>
-                            <svg width="22" height="12" viewBox="0 0 22 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989592 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM0 6.75L21 6.75V5.25L0 5.25L0 6.75Z" fill="black" />
-                            </svg>
-                        </span>
+
                     </div>
 
                     <div className="relative w-[60%] h-full rounded-3xl overflow-hidden">
