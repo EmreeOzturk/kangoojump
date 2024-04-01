f1f4e088b4393e047e35ded8b995b8b0f2533762
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-01 07:20:40 +0300

    Add AquaBikePage component

---
+++ app/(site)/activities/aqua-bike/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const AquaBikePage = () => {
+    return (
+        <div>
+            <div>
+                <h1>AQUA BIKE</h1>
+                <h2>Hijyen ve Güvenlik İlkeleri</h2>
+                <p>Aqua Bike aktiviteleri, profesyonel ve lisanslı eğitmenler tarafından sağlanmaktadır. İtalyan tasarıma sahip su bisikleti, renkli ABS kapağı ile estetik bir görünüm sunarken aynı zamanda güvenli bir kullanım sağlar. Pedal çevirme rahatlığı için çıplak ayaklarla kullanılabilmektedir.</p>
+                <h2>Dayanıklılık</h2>
+                <p>
+                    Paslanmaz çelik AISI 316 L gövdeye sahip olan su bisikleti, dayanıklılığı ile öne çıkar. Hem ön hem de arka dengeyi destekleyen dört adet vantuz ve ön alt kısımda bulunan iki tekerlek, dayanıklı ve stabil bir kullanım sunar. Uzun ömürlü malzemelerle üretilmiş olup, havuza indirip çıkartma işlemlerinde kolaylık sağlar.
+                </p>
+                <h2>
+                    Rehabilitasyon ve Fitness
+                </h2>
+                <p>
+                    Su bisikleti, kullanım su derinliği ayarlanabilir özellikleri sayesinde rehabilitasyon ve fitness amaçlı kullanıma uygundur. Gidon ve sele ayarlanabilir özelliklere sahiptir, böylece kullanıcıların ihtiyaçlarına uygun bir deneyim sunar. Spor yaparken suyun direnci, kasları güçlendirir ve rehabilitasyon süreçlerine katkı sağlar. Bu özellikleriyle Aqua Bike, hem fiziksel formu korumak hem de rehabilitasyon süreçlerine destek olmak için ideal bir seçenektir.
+                </p>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default AquaBikePage
