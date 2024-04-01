bb2f16d8ca5d3470d8d5119b89c5f109bfe3aa85
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-01 07:19:25 +0300

    Add FooterCarrier component

---
+++ app/components/footer/FooterCarrier.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterCarrier = () => {
+    return (
+        <div className='flex flex-col items-start justify-center w-1/4'>
+            <h3 className='font-bold text-3xl mb-10'>KARIYER</h3>
+        </div>
+    )
+}
+
+export default FooterCarrier
