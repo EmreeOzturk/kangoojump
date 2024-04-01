1370143175d45ef90a67515ed6449f9bfd55a818
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-01 07:21:25 +0300

    Add FlipBookPDF component to Catalog page

--- app/(site)/catalog/page.tsx
+++ app/(site)/catalog/page.tsx
@@ -1,8 +1,11 @@
+import FlipBookPDF from '@/app/components/FlipBookPDF'
 import React from 'react'
 
 const Catalog = () => {
     return (
-        <div>Catalog</div>
+        <div>
+            <FlipBookPDF />
+        </div>
     )
 }
 
