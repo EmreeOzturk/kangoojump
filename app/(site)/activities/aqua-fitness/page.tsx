a515b0067ede4c0dc226b0a0c64b254d16565f04
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-01 07:20:46 +0300

    Add Aqua Fitness page component

---
+++ app/(site)/activities/aqua-fitness/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const AquaFitnessPage = () => {
+    return (
+        <div>
+            <div>
+                <h1>AQUA FITNESS</h1>
+                <h2>Aqua Active – Aqua Fitness: Sağlıklı Yaşamın Sırrı</h2>
+                <p>
+                    Aqua Active olarak sunduğumuz Aqua Fitness aktiviteleri, uzman profesyoneller ve lisanslı eğitmenlerimiz tarafından yönetilmektedir. Bu etkileyici aktiviteler, suyun direncini kullanarak sağlıklı yaşamı desteklemeyi amaçlar. Aynı özeni, hijyen ve güvenlik standartlarına da taşıyoruz.
+                </p>
+                <h2>Profesyonel Eğitim ve Deneyim</h2>
+                <p>
+                    Aqua Fitness aktivitelerimiz, alanında uzmanlaşmış eğitmen kadromuz tarafından gerçekleştirilmektedir. Sağlıklı yaşam hedeflerinize ulaşmanıza rehberlik etmek için profesyonel deneyimimizi kullanıyoruz.
+                </p>
+                <h2>Yüksek Kaliteli Ekipmanlar ve Hijyen Standartları</h2>
+                <p>
+                    Aqua Fitness aktivitelerimiz için kullanılan ekipmanlar, CE belgesine sahip paslanmaz çelik malzemelerden üretilmiştir. Aynı zamanda Aqua Fitness dünyasında hijyen standartlarına da önem veriyoruz. Kullanılan dumbeller, su aktiviteleri için ideal olan yumuşak tutuşlu PE foam malzemeden üretilmiştir. Bu malzeme, su içinde hafif bir tutuş sağlar ve su altına girdiklerinde hijyen standartlarına uygun bir deneyim sunar.
+                </p>
+                <h2>Su Direnciyle Etkili Antrenman ve Ergonomik Tasarım</h2>
+                <p>Aqua Fitness aktiviteleri, suyun direncini kullanarak vücutta hafif bir etki yaratır. Bu, kasları güçlendirir, dayanıklılığı artırır ve tonusunuzu geliştirir. Yumuşak tutuşlu sert köpük dumbellerin ergonomik tasarımı, kullanıcıların su altında etkili bir şekilde çalışma yapmalarına uygun olacak şekilde düşünülmüş, pratik bir yaklaşıma sahiptir.</p>
+                <h2>Rehabilitasyon ve Fitness İçin Uygun</h2>
+                <p>
+                    Aqua Fitness, hem rehabilitasyon hem de fitness egzersizleri için uygundur. Su direncini kullanmak, eklemleri korurken aynı zamanda kalori yakımını artırır. Aqua fitness aktiviteleri, profesyonel ve lisanslı eğitmenler tarafından verilerek rehabilitasyon ve fitness egzersizleri için ideal bir çözüm sunar.
+                </p>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default AquaFitnessPage
