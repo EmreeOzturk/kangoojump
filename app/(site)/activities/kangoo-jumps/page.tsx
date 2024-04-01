42365d4aebf3c64a0b33b76737afb0a577438eaf
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-01 07:21:02 +0300

    Add Kangoo Jumps activity page

---
+++ app/(site)/activities/kangoo-jumps/page.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const KangooJumpPage = () => {
+    return (
+        <div>
+            <div>
+                <h1>
+                    KANGOO JUMPS
+                </h1>
+                <h2>
+                    Kangoo Jumps: Sağlığınıza Yepyeni Bir Yaklaşım
+                </h2>
+                <p>
+                    Kangoo Jumps aktivitesi, uzman ve lisanslı eğitmenlerimiz tarafından profesyonelce yönetilmektedir. Aqua Active’in sağladığı bu enerji dolu aktivitede, Kangoo Jumps’ın orijinal ve patentli ürünleri kullanılarak, hem eğlenceli hem de etkili bir antrenman deneyimi sunulmaktadır.
+                </p>
+                <h2>
+                    Fiziksel Rahatlıklar Üzerindeki Etkileri
+                </h2>
+                <p>
+                    Kangoo Jumps, birçok fiziksel rahatsızlık üzerinde olumlu etkiler sağlar. Kardiyovasküler sistemden lenfatik sisteme, kan dolaşımından metabolizmanın düzenlenmesine kadar geniş bir yelpazede etkileşimde bulunur. Trigliserit, kolesterol, şeker hastalığı ve tansiyon gibi sorunlarda da pozitif etkiler sunar. Ayrıca, duruş bozukluklarına karşı da destek sağlar.
+                </p>
+                <h2>
+                    Mental Sağlık ve Koordinasyon Yeteneği
+                </h2>
+                <p>
+                    Sadece fiziksel sağlığa değil, Kangoo Jumps aynı zamanda mental sağlığa da katkıda bulunur. Uyku problemlerini azaltır, koordinasyon yeteneğini artırır ve beyin fonksiyonlarını hızlandırır. Dikkat artışını düzenler ve geliştirir. Bu, günlük yaşantınıza daha odaklı ve enerjik bir şekilde yaklaşmanızı sağlar.
+                </p>
+                <h2>
+                    Kemik Sağlığı ve Kalori Yakımı
+                </h2>
+                <p>
+                    Kangoo Jumps aktivitesi, tüm vücutta etkili bir şekilde çalışarak aylık %3 oranında kemik gelişimine katkıda bulunur. Bu, kemik sağlığını desteklemenin yanı sıra genel vücut tonusunu da artırır. Başlangıçta 1 saatte 600 ile 800 kcal/h yakılabilirken, 2-3 ay içinde bu rakam 1200-1500 kcal/h’ye çıkabilir.
+                    Sağlıklı yaşam için atılan her adım, Aqua Active’in sunduğu Kangoo Jumps ile daha eğlenceli ve etkili hale geliyor. Siz de bu benzersiz aktiviteyle tanışın ve sağlığınıza yeniden bir enerji katın!
+                </p>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default KangooJumpPage
