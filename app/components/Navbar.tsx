8c9700decc80a2b59e4bce1f0192e3d5f86f48f9
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-01 11:26:27 +0300

    Add dropdown menu to Navbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,38 +1,86 @@
 "use client";
-import React from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation';
 import { clsx } from 'clsx';
+
+type DropdownProps = {
+    setShow: (show: boolean) => void
+}
+
+const Dropdown: React.FC<DropdownProps> = (
+    {
+        setShow
+    }
+) => {
+    return (
+        <div
+            onMouseLeave={() => setShow(false)}
+            className='absolute top-14 border border-white rounded-lg -left-32 gap-2 bg-gray-800/50 w-80 text-white flex flex-col items-start justify-start p-4'>
+            <a className='activities-item' href="/activities/kangoo-jumps">
+                Kangoo Jumps
+            </a>
+            <a className='activities-item' href="/activities/aqua-jump">
+                Aqua Jump
+            </a>
+            <a className='activities-item' href="/activities/aqua-fitness">
+                Aqua Fitness
+            </a>
+            <a className='activities-item' href="/activities/aqua-bike">
+                Aqua Bike
+            </a>
+            <a className='activities-item' href="/activities/aqua-step">
+                Aqua Step
+            </a>
+        </div>
+    )
+}
+
 const Navbar = () => {
+    const [show, setShow] = useState(false)
     const pathname = usePathname()
     return (
         <nav className='flex gap-8 items-center text-md font-bold uppercase tracking-[1px]'>
             <div className='group'>
-                <Link className={clsx(pathname === "/about" && "text-[#C4FE01]")} href="/about" scroll={false}>
+                <a
+                    className={clsx(pathname === "/about" && "text-[#C4FE01]")} href="/about">
                     Hakkımızda
-                </Link>
+                </a>
                 <div
                     className='h-1 w-0 bg-[#C4FE01] rounded-full group-hover:w-full transition-all duration-500 mt-1'
                 ></div>
             </div>
             <div className='group'>
-                <Link className={clsx(pathname === "/catalog" && "text-[#C4FE01]")} href="/catalog" scroll={false}>
+                <Link
+                    onMouseEnter={
+                        () => { setShow(false) }
+                    }
+                    className={clsx(pathname === "/catalog" && "text-[#C4FE01]")} href="/catalog" >
                     Katalog
                 </Link>
                 <div
                     className='h-1 w-0 bg-[#C4FE01] rounded-full group-hover:w-full transition-all duration-500 mt-1'
                 ></div>
             </div>
-            <div className='group'>
-                <Link className={clsx(pathname === "/activities" && "text-[#C4FE01]")} href="/activities" scroll={false}>
+            <div className='group relative'>
+                <button
+                    onMouseEnter={
+                        () => { setShow(true) }
+                    }
+                    className={clsx("uppercase", pathname.startsWith('/activities') && "text-[#C4FE01]")}  >
                     Aktiviteler
-                </Link>
+                </button>
                 <div
                     className='h-1 w-0 bg-[#C4FE01] rounded-full group-hover:w-full transition-all duration-500 mt-1'
                 ></div>
+                {show && <Dropdown setShow={setShow} />}
             </div>
             <div className='group'>
-                <Link className={clsx(pathname === "/contact" && "text-[#C4FE01]")} href="/contact" scroll={false}>
+                <Link
+                    onMouseEnter={
+                        () => { setShow(false) }
+                    }
+                    className={clsx(pathname === "/contact" && "text-[#C4FE01]")} href="/contact" >
                     Iletisim
                 </Link>
                 <div
@@ -40,8 +88,7 @@ const Navbar = () => {
                 ></div>
             </div>
 
-
-        </nav>
+        </nav >
     )
 }
 
