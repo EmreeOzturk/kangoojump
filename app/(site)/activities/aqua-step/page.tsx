30c823053508a00a48d09f22efcfc1776543587b
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-01 07:20:57 +0300

    Add Aqua Step page component

---
+++ app/(site)/activities/aqua-step/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const AquaStepPage = () => {
+    return (
+        <div>
+            <div>
+                <h1>
+                    AQUA STEP
+                </h1>
+                <h2>Aqua Active – Aqua Step</h2>
+                <p>Aqua Active olarak sunmuş olduğumuz Aqua Step aktiviteleri, profesyonel ve lisanslı eğitmenlerimiz tarafından gerçekleştirilmektedir. Bu etkileyici aktivite için kullanılan ekipmanlar, paslanmaz çelik malzemelerden üretilmiş olup CE belgesine sahiptir.</p>
+                <h2>Hijyen ve Güvenlik Standartları</h2>
+                <p>Aqua Step aktiviteleri, profesyonel ve lisanslı eğitmenlerimiz tarafından yönetilmekte olup havuz hijyenine herhangi bir zarar vermez ve klor dengesini olumsuz etkilemez. Bu sayede kullanıcılar, temiz ve güvenli bir ortamda su sporlarının keyfini çıkarabilirler. Alt tarafında bulunan altı adet vantuzlar, aktivite sırasında sağlam bir duruş sağlayarak güvenlik standartlarını en üst düzeye çıkarır.</p>
+                <h2>Dayanıklılık ve Estetik Tasarım </h2>
+                <p>Aqua Step, polietilen malzeme kullanılarak üretilmiş olup dayanıklılığı ön planda tutar. Estetik tasarımıyla da dikkat çeken bu aktivite ekipmanı, 70 x 35 x h 19 cm boyutlarındadır ve 6 kg ağırlığı ile taşınabilir. Hem dayanıklılığı hem de şık görünümüyle Aqua Step, uzun ömürlü ve göz alıcı bir kullanım sunar.</p>
+                <h2>Rehabilitasyon ve Fitness İçin Özel Çözüm</h2>
+                <p>Aqua Step, alt tarafındaki vantuzlar sayesinde sabit duruş sağlamasıyla rehabilitasyon ve fitness egzersizleri için özel olarak tasarlanmıştır. Kullanım su derinliği 100 – 140 cm arasında değişmektedir, bu da farklı fitness seviyelerine uygun bir çözüm sunar. Hem eğlenceli hem de etkili olan Aqua Step, su direncini kullanarak vücut üzerinde hafif bir etki yaratır, böylece kullanıcılar eklem ve kaslarını güçlendirirken aynı zamanda kalori yakımını artırabilirler.</p>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default AquaStepPage
