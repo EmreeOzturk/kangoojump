8b67fbae0a77037b775227ee54b1d5704a0d1935
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-01 11:26:19 +0300

    Update Card component styling

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import { useTransform, motion, useScroll } from 'framer-motion';
 import { useRef } from 'react';
 import Link from 'next/link';
-
+import { HoverBorderGradient } from "../components/ui/hover-border-gradient";
 
 interface CardProps {
     feature: {
@@ -29,9 +29,9 @@ const Card: React.FC<CardProps> = (
     const imageScale = useTransform(scrollYProgress, [0, 1], [2, 1])
     const scale = useTransform(progress, range, [1, targetScale]);
     return (
-        <div ref={container} className='h-screen flex items-center justify-center sticky top-0'>
-            <motion.div style={{ scale, top: `calc(-5vh + ${i * 25}px)` }} className='flex flex-col relative -top-[25%] card text-white h-[600px] w-[80vw] rounded-2xl p-[50px] origin-top'>
-                <h2 className='text-center font-bold text-[32px]'>{feature.title}</h2>
+        <div ref={container} className='h-screen  flex items-center justify-center sticky top-0'>
+            <motion.div style={{ scale, top: `calc(-5vh + ${i * 25}px)` }} className='flex flex-col relative -top-[25%] card text-white h-[600px]  w-[80vw] rounded-2xl p-[50px] origin-top'>
+                <h2 className='text-center font-bold text-[36px]'>{feature.title}</h2>
                 <div className="flex items-start h-full mt-[50px] gap-12">
                     <div className="w-[40%]  relative">
                         <p className='text-xl tracking-wider first-letter:text-[32px] first-letter:font-serif'>{feature.description}</p>
