44695dda246723c1cbf89bf6046154a7e0128508
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-14 04:45:27 +0300

    refactor: Update FooterAboutUs component styles for better responsiveness

--- app/components/footer/FooterAboutUs.tsx
+++ app/components/footer/FooterAboutUs.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import Logo from '../Logo'
+import React from "react";
+import Logo from "../Logo";
 const FooterAboutUs = () => {
-    return (
-        <div className='flex flex-col items-start justify-center p-4 md:p-0 md:w-1/4'>
-            <h3 className='font-bold text-3xl mb-10'>HAKKIMIZDA</h3>
-            <Logo />
-            <p>
-                Alanya merkezli kuruluşumuz, 2023 yılında kurulan ve spor ile sağlıklı yaşamı birleştiren benzersiz bir misyonla hareket eden bir kuruluştur.
-            </p>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col items-start justify-center p-4 md:p-0 md:w-1/4">
+      <h3 className="font-bold text-3xl ">HAKKIMIZDA</h3>
+      <Logo size="xs" />
+      <p>
+        Alanya merkezli kuruluşumuz, 2023 yılında kurulan ve spor ile sağlıklı
+        yaşamı birleştiren benzersiz bir misyonla hareket eden bir kuruluştur.
+      </p>
+    </div>
+  );
+};
 
-export default FooterAboutUs
+export default FooterAboutUs;
