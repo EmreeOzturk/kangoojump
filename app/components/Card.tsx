6e34fc1509e7746c3dcfb4621e3f9c7e8aaf6844
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-14 04:45:11 +0300

    refactor: Update Card component styles for better responsiveness

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -28,7 +28,7 @@ const Card: React.FC<CardProps> = (
     const scale = useTransform(progress, range, [1, targetScale]);
     return (
         <div ref={container} className='h-screen  flex items-center justify-center sticky top-32 md:top-0'>
-            <motion.div style={{ scale, top: `calc(-5vh + ${i * 25}px)` }} className='flex  flex-col relative -top-[25%] card text-white md:h-[600px] h-screen w-[90vw]  md:w-[80vw] rounded-2xl p-[50px] origin-top'>
+            <motion.div style={{ scale, top: `calc(-5vh + ${i * 25}px)` }} className='flex  flex-col relative -top-[25%] card text-white md:h-[690px] h-screen w-[90vw]  md:w-[80vw] rounded-2xl p-[50px] origin-top'>
                 <h2 className='text-center font-bold text-[24px] md:text-[36px]'>{feature.title}</h2>
                 <div className="flex flex-col md:flex-row items-start h-full mt-[50px] gap-12">
                     <div className=" w-[70vw] flex justify-center items-center md:w-[40%]  relative">
