f26fd03397fa033884acfde35090d65afdfb8a1c
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-14 06:10:57 +0300

    refactor: Update Referances component with lazy loading for images

--- app/components/Referances.tsx
+++ app/components/Referances.tsx
@@ -1,36 +1,8 @@
 import React from 'react'
 import { brands } from '../brands'
-import Image from 'next/image'
 import { InfiniteMovingCards } from './ui/infinite-moving-cards';
 const Referances = () => {
     return (
-      // <div className='w-screen h-[300px] flex items-center justify-center'>
-      //     {/* <div className='w-full h-fit flex items-center justify-center flex-col border-4'> */}
-      //     <div className='flex w-[1800px] select-none brand'>
-      //         <div className='flex-shrink-0 flex items-center justify-around whitespace-nowrap w-full '>
-      //             {brands.map((brand, i) => {
-      //                 return (
-      //                     <div key={`b_${i}`} className='grid  place-items-center rounded-full w-[300px] h-[300px] relative'>
-      //                         <Image className='object-contain aspect-auto rounded-full' fill src={brand.image} alt={brand.alt} />
-      //                     </div>
-      //                 )
-      //             })
-      //             }
-      //         </div>
-      //         <div className='flex-shrink-0 flex items-center justify-around whitespace-nowrap w-full '>
-      //             {
-      //                 brands.map((brand, i) => {
-      //                     return (
-      //                         <div key={`b_${i}`} className='grid  place-items-center rounded-full w-[300px] h-[300px] relative'>
-      //                             <Image className='object-contain aspect-auto rounded-full ' fill src={brand.image} alt={brand.alt} />
-      //                         </div>
-      //                     )
-      //                 })
-      //             }
-      //         </div>
-      //     </div>
-      //     {/* </div> */}
-      // </div>
         <InfiniteMovingCards
           items={brands}
           direction="right"
