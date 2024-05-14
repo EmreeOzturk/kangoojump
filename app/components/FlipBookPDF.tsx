68399a07e8dc38e0df7b6e112183114c526aea2f
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-14 04:45:14 +0300

    feat: Improve PDF viewer component with responsive layout and navigation buttons

--- app/components/FlipBookPDF.tsx
+++ app/components/FlipBookPDF.tsx
@@ -1,98 +1,103 @@
-"use client"
-import { useCallback, useState } from 'react';
-import { Document as Doc, Page as Pag, pdfjs } from 'react-pdf';
+"use client";
+import { useCallback, useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
 
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import 'react-pdf/dist/Page/TextLayer.css';
-import { useResizeObserver } from '@wojtekmaj/react-hooks';
-// import file from '/static/catalog.pdf';
-import type { PDFDocumentProxy } from 'pdfjs-dist';
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+import { useResizeObserver } from "@wojtekmaj/react-hooks";
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 const options = {
-    cMapUrl: '/cmaps/',
-    standardFontDataUrl: '/standard_fonts/',
+  cMapUrl: "/cmaps/",
+  standardFontDataUrl: "/standard_fonts/",
 };
 const resizeObserverOptions = {};
-
-const maxWidth = 500;
 
 type PDFFile = string | File | null;
 
 const FlipBookPDF = () => {
-    const [numPages, setNumPages] = useState<number>();
-    const [pageNumber, setPageNumber] = useState<number>(1);
-    const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
-    const [containerWidth, setContainerWidth] = useState<number>();
-    const [file, setFile] = useState<PDFFile>('/static/catalog.pdf');
+  const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
+  const [containerWidth, setContainerWidth] = useState<number>();
+  const [file, setFile] = useState<PDFFile>("/static/catalog.pdf");
+  const [pageWidth, setPageWidth] = useState(0);
 
+  const onResize = useCallback<ResizeObserverCallback>((entries) => {
+    const [entry] = entries;
 
-    const onResize = useCallback<ResizeObserverCallback>((entries) => {
-        const [entry] = entries;
-
-        if (entry) {
-            setContainerWidth(entry.contentRect.width);
-        }
-    }, []);
-
-    useResizeObserver(containerRef, resizeObserverOptions, onResize);
-
-
-    function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy): void {
-        setNumPages(nextNumPages);
+    if (entry) {
+      setContainerWidth(entry.contentRect.width);
     }
-    return (
-        <div className="Example__container select-none">
-            <div className="Example__container__document " ref={setContainerRef}>
-                <Doc className="relative" file={file} onLoadSuccess={onDocumentLoadSuccess as any} options={options}>
-                    <div className='relative  h-screen'>
-                        <Pag
-                            pageNumber={pageNumber}
-                            width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
-                        />
-                        <button
-                            style={{
-                                // display: pageNumber === 1 ? 'none' : 'block',
-                                position: 'absolute',
-                                width: "32px",
-                                height: "32px",
-                                bottom: '50%',
-                                left: '-35px',
-                                backgroundColor: "white",
-                                color: "black",
-                                borderRadius: "50%"
-                            }}
-                            onClick={
-                                () => {
-                                    setPageNumber(pageNumber - 1)
-                                }
-                            }>
-                            {"<"}
-                        </button>
-                        <button
-                            style={{
-                                // display: pageNumber === 9 ? 'none' : 'block',
-                                position: 'absolute',
-                                width: "32px",
-                                height: "32px",
-                                bottom: '50%',
-                                right: '-35px',
-                                backgroundColor: "white",
-                                color: "black",
-                                borderRadius: "50%"
-                            }}
-                            onClick={
-                                () => {
-                                    setPageNumber(pageNumber + 1)
-                                }
-                            }>
-                            {">"}
-                        </button>
-                    </div>
-                </Doc>
-            </div>
-        </div>
-    )
-}
+  }, []);
 
-export default FlipBookPDF
+  useResizeObserver(containerRef, resizeObserverOptions, onResize);
+
+  function onDocumentLoadSuccess({
+    numPages: nextNumPages,
+  }: {
+    numPages: number;
+  }) {
+    setNumPages(nextNumPages);
+  }
+
+  function onPageLoadSuccess() {
+    setPageWidth(window.innerWidth);
+  }
+
+  return (
+    <div className="h-screen w-screen flex justify-center items-center rounded-2xl gap-4 ">
+      <button
+        disabled={pageNumber === 1}
+        className="bg-zinc-600 text-[#C4FE01] rounded-full w-8 h-8 flex justify-center items-center
+          disabled:bg-gray-400 disabled:text-gray-200 disabled:cursor-not-allowed
+          hover:bg-zinc-700 hover:text-[#C4FE01] hover:cursor-pointer
+          transition-all duration-300 ease-in-out"
+        onClick={() => {
+          if (pageNumber === 1) {
+            return;
+          }
+          setPageNumber(pageNumber - 1);
+        }}
+      >
+        {"<"}
+      </button>
+      <Document
+        file={file}
+        onLoadSuccess={onDocumentLoadSuccess}
+        options={options}
+        renderMode="canvas"
+        className="relative rounded-2xl overflow-hidden"
+      >
+        <Page
+          className="rounded-2xl"
+          key={pageNumber}
+          pageNumber={pageNumber}
+          renderAnnotationLayer={false}
+          renderTextLayer={false}
+          onLoadSuccess={onPageLoadSuccess}
+          width={Math.max(pageWidth * 0.4, 200)}
+        />
+      </Document>
+
+      <button
+        className="bg-zinc-600 text-[#C4FE01] rounded-full w-8 h-8 flex justify-center items-center
+          disabled:bg-gray-400 disabled:text-gray-200 disabled:cursor-not-allowed
+          hover:bg-zinc-700 hover:text-[#C4FE01] hover:cursor-pointer
+          transition-all duration-300 ease-in-out
+        "
+        disabled={pageNumber === numPages}
+        onClick={() => {
+          if (pageNumber === numPages) {
+            return;
+          }
+          setPageNumber(pageNumber + 1);
+        }}
+      >
+        {">"}
+      </button>
+    </div>
+  );
+};
+
+export default FlipBookPDF;
