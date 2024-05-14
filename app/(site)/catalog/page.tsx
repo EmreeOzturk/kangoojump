bbbf96c9e23efed42ec7ed5d12a073b243579adf
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-14 04:15:32 +0300

    chore: Refactor Catalog page component

--- app/(site)/catalog/page.tsx
+++ app/(site)/catalog/page.tsx
@@ -1,12 +1,11 @@
-import FlipBookPDF from '@/app/components/FlipBookPDF'
-import React from 'react'
+import FlipBookPDF from "@/app/components/FlipBookPDF";
 
 const Catalog = () => {
-    return (
-        <div className='h-[100vh] py-8 mb-32 '>
-            <FlipBookPDF />
-        </div>
-    )
-}
+  return (
+    <div className="h-[100vh] py-8 mb-32  ">
+      <FlipBookPDF />
+    </div>
+  );
+};
 
-export default Catalog
+export default Catalog;
