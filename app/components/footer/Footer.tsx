8f67a89df2af86774c2e857ba582253043ed2b5e
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-14 04:45:24 +0300

    refactor: Update Footer component styles for better responsiveness

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,16 +1,16 @@
-import FooterActivities from './FooterActivities';
-import FooterCarrier from './FooterCarrier';
-import FooterContactUs from './FooterContactUs';
-import FooterAboutUs from './FooterAboutUs';
+import FooterActivities from "./FooterActivities";
+import FooterCarrier from "./FooterCarrier";
+import FooterContactUs from "./FooterContactUs";
+import FooterAboutUs from "./FooterAboutUs";
 const Footer = () => {
-    return (
-        <div className='md:h-80 bg-black/40 text-slate-300 flex flex-col md:flex-row items-center justify-between md:px-24 gap-6 md:gap-12'>
-            <FooterAboutUs />
-            <FooterActivities />
-            {/* <FooterCarrier /> */}
-            <FooterContactUs />
-        </div>
-    )
-}
+  return (
+    <div className="md:h-80 bg-black/40 text-slate-300 flex flex-col md:flex-row items-start pt-5 justify-between md:px-24 gap-6 md:gap-12">
+      <FooterAboutUs />
+      <FooterActivities />
+      {/* <FooterCarrier /> */}
+      <FooterContactUs />
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
