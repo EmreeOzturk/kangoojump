6db6c6393f59a24790bfeb3292d49bfba5fc6864
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-14 08:01:21 +0300

    refactor: Remove unused FooterCarrier and HoverBorderGradient components

--- app/components/footer/FooterCarrier.tsx
+++
@@ -1,9 +0,0 @@
-const FooterCarrier = () => {
-    return (
-        <div className='flex flex-col items-start justify-center w-1/4'>
-            <h3 className='font-bold text-3xl mb-10'>KARIYER</h3>
-        </div>
-    )
-}
-
-export default FooterCarrier
